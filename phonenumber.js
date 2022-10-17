bc18505cdf8e7831a886a0a5bbf45b1dce16d6cb
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-10-16 23:13:50 -0500

    done with error catch

--- phonenumber.js
+++ phonenumber.js
@@ -9,8 +9,16 @@ The returned format must be correct in order to complete this challenge.
 Don’t forget the space after the closing parentheses!
 */
 
-function phoneNumberCreator(arr) {
-    const firstThird = arr[0] + arr[1] + arr[2];
-    const secondThird = arr[3] + arr[4] + arr[5];
-    const thirdThird = arr[6] + arr[7] + arr[8];
+function createPhoneNumber(arr) {
+    if (arr.length !== 10) {
+        console.log("Phone number must have 10 digits.")
+    } else {
+        const firstThreeDigits = arr.slice(0,3).join("");
+        const secondThreeDigits = arr.slice(3,6).join("");
+        const lastFourDigits = arr.slice(6,10).join("");
+
+        return `(${firstThreeDigits}) ${secondThreeDigits}-${lastFourDigits}`;
+    }
 }
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
